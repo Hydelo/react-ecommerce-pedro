1e2096cffcacb48efa78345432dc5c8ff640124b
Hydelo Wagner <hydelo_w@yahoo.com.br>
2026-06-23 10:34:34 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Aula do youtube: **ReactJS Full Course 2026 | Build and Deploy a Beginner Ecommerce Website using React**
+
+Do canal: **PedroTech**
+
+Executado em 23/06/2026 para rever os conceitos de React
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function Greeting() {
+  return <h1>Hello, I am Hydelo</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <Greeting />
+    </div>
+  );
+}
+
+export default App;
